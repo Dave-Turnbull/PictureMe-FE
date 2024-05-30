c5e09c24cfba6ee3a317b3a06a98620fb4572627
Dave Turnbull <dav-tu@hotmail.com>
2024-05-30 10:26:25 +0100

    fixed user guessing

--- routes/GuessThePicture.tsx
+++ routes/GuessThePicture.tsx
@@ -32,7 +32,7 @@ export const GuessThePicture = ({ route, navigation }) => {
 
   const submitGuess = async () => {
     let score = 0;
-
+    console.log(chosenUserID, 'chosen user id', picture.userID, 'picture.userID')
     if (chosenUserID === picture.userID) {
       console.log("correct!");
       score++;
@@ -58,7 +58,7 @@ export const GuessThePicture = ({ route, navigation }) => {
         if (itUser.userID !== userData.user.userID) {
           return (
             <View key={itUser.id}>
-              <Button onPress={() => setChosenUserID(itUser)}>
+              <Button onPress={() => setChosenUserID(itUser.userID)}>
                 {itUser.username}
               </Button>
             </View>
